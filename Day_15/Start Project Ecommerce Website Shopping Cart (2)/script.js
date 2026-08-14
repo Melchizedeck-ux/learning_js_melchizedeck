8d517895a06952e753410b5dda4737a3c8674851
Melchizedeck-ux <tayenson77@gmail.com>
2026-08-14 10:56:42 +0100

    Added more variables on my javascript

--- Day_15/Start Project Ecommerce Website Shopping Cart (2)/script.js
+++ Day_15/Start Project Ecommerce Website Shopping Cart (2)/script.js
@@ -4,3 +4,32 @@ const cartClose = document.querySelector("#cart-close");
 cartIcon.addEventListener("click", () => cart.classList.add("active"));
 cartClose.addEventListener("click", () => cart.classList.remove("active"));
 const addCartButtons = document.querySelectorAll(".add-cart");
+
+addCartButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        const productBox = event.target.closes(".product-box");
+        addToCart(productBox);
+    });
+});
+
+const cartContent = document.querySelector(".cart-content");
+const addToCart = productBox => {
+    const producImgSrc = productBox.querySelector("img").src;
+    const productTitle = productBox.querySelector(".product-title").textContent;
+    conts productPrice = productBox.querySelector(".price").textContent;
+    const cartBox = document.createElement("div");
+    cartBox.classList.add("cart-box");
+    cartBox.innerHTML = `   <img src="images/product1.jpg" class="cart-img">
+                <div class="cart-detail">
+                    <h2 class="cart-product-title">Cusual Black Polo</h2>
+                    <span class="cart-price">$100</span>
+                    <div class="cart-quantity">
+                        <button id="decrement">-</button>
+                        <span class="number">1</span>
+                        <button id="increment">+</button>
+                    </div>
+                </div>
+
+                <img src="images/dustbin.png" class="cart-remove" style="width: 30px; height: 30px;">
+                `
+};
