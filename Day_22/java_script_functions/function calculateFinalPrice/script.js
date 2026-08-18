e3a43f8d11ba9df5c73e0d892fd9a6b83ab87cde
Melchizedeck-ux <tayenson77@gmail.com>
2026-08-18 11:49:26 +0100

    completed the funvtion and ran it

--- Day_22/java_script_functions/function calculateFinalPrice/script.js
+++ Day_22/java_script_functions/function calculateFinalPrice/script.js
@@ -4,4 +4,8 @@ function CalculateFinalPrice(price, itemName) {
     let discount = 0.2;
     let deliveryFee = 5;
     let total = (price * (1 - discount)) * (1 + taxRate) + deliveryFee;
+    console.log(`Final price for ${itemName} is $${total}`)
 }
+CalculateFinalPrice(80, "shoes");
+CalculateFinalPrice(150, "bags");
+CalculateFinalPrice(10, "bags");
