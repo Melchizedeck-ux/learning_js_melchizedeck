6ea0589fd4b1460c2776719b1587cc20fef84f4a
Melchizedeck-ux <tayenson77@gmail.com>
2026-08-31 12:15:15 +0100

    Learn how to assing multiple value to a variable

--- week_7_day_1/Revision on java script/script.js
+++ week_7_day_1/Revision on java script/script.js
@@ -2,5 +2,12 @@ console.log("I love javascript");
 console.log('Javascript is fun');
 console.log(5);
 console.log(80.5);
+
 let language = "javascript";
 console.log(language);
+
+
+let name = "Melchizedeck";
+console.log(name);
+let name = "Tayen"
+console.log(name);
